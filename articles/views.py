3b7a446f9217dee1109687e4fbeb9c910a88f83c
junbeompark <a01077348062@gmail.com>
2023-08-24 16:21:34 +0900

    Read 중 index 구현

---
+++ articles/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Article
+
+# Create your views here.
+
+# Read 함수
+def index(request):
+    article = Article.objects.all()
+    
+    context = {
+        'article': article
+    }
+    
+    return render(request, 'index.html', context)
